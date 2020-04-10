850c43760aa76043ad11561379b655108ad615a6
Satoh-K37 <skohcode0385@gmail.com>
2020-04-10 23:51:39 +0900

    タスク編集機能を実装。例のごとく基本的な感じだけ。 次は新規と編集をパーシャルで共通化する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,7 +21,15 @@ class TasksController < ApplicationController
 
 
   def edit
+    @task = Task.find(params[:id])
   end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.title}」を更新しました"
+  end
+
 
   private
   
