46593fbfa89664a967e63080afebeee69590d381
Satoh-K37 <skohcode0385@gmail.com>
2020-04-16 14:44:00 +0900

    System Specを書くための準備完了

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,16 @@
 module UsersHelper
 
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+  # def log_in(user)
+  #   session[:user_id] = user.id
+  # end
 
-  #ユーザ検索の共通化の処理に使ってる
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # #ユーザ検索の共通化の処理に使ってる
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
   
-  def user_params
-    params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
-  end
+  # def user_params
+  #   params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+  # end
 
 end
