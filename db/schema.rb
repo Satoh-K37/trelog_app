ff5212d043fddb03e246994aa462b8e75e2ddcab
Satoh-K37 <skohcode0385@gmail.com>
2020-05-03 03:06:06 +0900

    statusの型変更のマイグレーション が消えていたので再度作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_190831) do
+ActiveRecord::Schema.define(version: 2020_05_02_180014) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_190831) do
     t.integer "set_count"
     t.text "memo"
     t.date "deadline"
-    t.integer "status"
+    t.boolean "status", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
