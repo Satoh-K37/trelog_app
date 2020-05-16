de92b70319f5b88e54d0cb6be4b235ec1a4f7712
Satoh-K37 <skohcode0385@gmail.com>
2020-05-16 22:40:36 +0900

    ボタンで未完了のタスクのステータスを完了に切り替えられるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -76,31 +76,53 @@ class TasksController < ApplicationController
   #   # .includes(:user)
   #   # render :index
   # end
-  
-  # タスクのステータスを更新させる処理（Ajaxアクション）
-  def task_status
-    # 対処のタスクの検索はset_taskで共通化済み
-    if @task.status == true
-      @task.update(status: 'done')
-      redirect_to done_tasks_path, notice: "タスク「#{@task.title}」を未完了にします"
-    else
-      @task.update(status: 'status')
-      redirect_to todo_tasks_path, notice: "タスク「#{@task.title}」を完了しました"
-    end
-  end
 
+  # def deadline_count(task)
+  #   # 現在の日付を取得し、変数に代入
+  #   today = Date.today
+  #   # 期限日を変数に代入する
+  #   deadline = task.deadline
+  #   # 期限日 - 現在の日付を計算してタスク終了期限日までの日数を割り出す。
+  #   deadline_alert = (deadline - today).numerator
+
+  #   # 期限日と現在の日付に差がない場合
+  #   if deadline_alert == 0
+  #     "今日が期限日のタスクです"
+  #     # 期限日が残り1〜３日になった場合
+  #   elsif deadline_alert.between?(1, 3)
+  #     "期限日まで残り#{deadline_alert}日"
+  #     # それ以外の場合
+  #   else
+  #     "期限日: #{task.deadline}"
+  #   end
+  # end
+
+    # タスクのステータスを更新させる処理（Ajaxアクション）
+    def task_status
+      # 対処のタスクの検索はset_taskで共通化済み
+      if @task.status == true
+        @task.update(status: 'done')
+        redirect_to done_tasks_path, notice: "タスク「#{@task.title}」を未完了にします"
+      else
+        @task.update(status: 'status')
+        redirect_to todo_tasks_path, notice: "タスク「#{@task.title}」を完了しました"
+      end
+    end
   
-  # プログレスバーで当日のタスクの進捗率を割り出すためのメソッド。
-  # 忘れないようにメモ。あくまで案なので実際にやってみないとわからん
-  def task_progress
-    # １、当日に作成されたタスクを探して変数today_taskに代入(当日の判定の部分がまだ曖昧。ここ考える)
-    # ２、当日のタスクの中でステータスが完了になっているものを変数done_taskに代入
-    # ３、progress_result = today_task % done_task
-    # ４、progress_resultをreturnで返す。
-    # ５、viewでtask_progressを呼び出すとprogress_resultの値がくるはずなので、
-    #    それをプログレスバーのvalueとして使えばタスク進捗率が出来上がるはず
-    # 
-  end
+    
+    # プログレスバーで当日のタスクの進捗率を割り出すためのメソッド。
+    # 忘れないようにメモ。あくまで案なので実際にやってみないとわからん
+    def task_progress
+      # １、当日に作成されたタスクを探して変数today_taskに代入(当日の判定の部分がまだ曖昧。ここ考える)
+      # ２、当日のタスクの中でステータスが完了になっているものを変数done_taskに代入
+      # ３、progress_result = today_task % done_task
+      # ４、progress_resultをreturnで返す。
+      # ５、viewでtask_progressを呼び出すとprogress_resultの値がくるはずなので、
+      #    それをプログレスバーのvalueとして使えばタスク進捗率が出来上がるはず
+      # 
+    end
+
+
 
 
 
